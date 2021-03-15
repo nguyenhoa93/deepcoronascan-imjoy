cbdb9ca83fc78f06f2d820c34c16f2f30fd8d13b
Hoa Nguyen <ntthoa.uphcm@gmail.com>
2021-03-16 00:00:17 +0100

    add data uploader

---
+++ src/configs.py
@@ -0,0 +1,6 @@
+BASE_URL = "https://ai.pasteur.fr"
+TOKEN = "<your_token>"
+DATASET_ID = "<your_dataset_id"
+DATASET_DIR = "<local_dataset_directory>"
+UPLOAD_FILES = ["image.dm3"]
+CONVERT_FILES = {"image.dm3": "image.ome.tif"}
