bade42b6b10f1008a66b0f43c2908a005985013a
Hoa Nguyen <ntthoa.uphcm@gmail.com>
2021-03-16 00:10:51 +0100

    rm configs.py

--- src/configs.py
+++
@@ -1,6 +0,0 @@
-BASE_URL = "https://ai.pasteur.fr"
-TOKEN = "<your_token>"
-DATASET_ID = "<your_dataset_id"
-DATASET_DIR = "<local_dataset_directory>"
-UPLOAD_FILES = ["image.dm3"]
-CONVERT_FILES = {"image.dm3": "image.ome.tif"}
