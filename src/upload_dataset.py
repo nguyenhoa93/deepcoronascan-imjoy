8756b9177514b3c0485ede2731a3f20cd2fa0a41
Hoa  NGUYEN THI THANH <hoa.nguyen-thi-thanh@pasteur.fr>
2021-04-01 22:32:54 +0200

    add color

--- src/upload_dataset.py
+++ src/upload_dataset.py
@@ -1,3 +1,24 @@
 """
 Dataset uploader 
 """
+import os
+import time
+import requests
+import zipfile
+from configs import *
+
+# %% Connect to task server
+# listing tasks
+response = requests.get(
+    BASE_URL + "/tasks", headers={"Authorization": f"Bearer {TOKEN}"}
+)
+response_obj = response.json()
+assert (
+    response_obj.get("success") == True
+), f"Failed to requesting URL for upload, error: {response_obj.get('detail')}"
+tasks = response_obj["result"]
+
+assert (
+    DATASET_ID in tasks
+), f"Task {DATASET_ID} not found, must be one of: {list(tasks.keys())}"
+
